71c4534c52932b5251c6bbbd9c3288496a567e53
hminaee-tc <hminaee@tucows.com>
2024-06-12 09:34:16 -0300

    feat/findReplace find and replace

--- find_and_replace/main.py
+++ find_and_replace/main.py
@@ -1,4 +1,35 @@
 # -*- coding: utf-8 -*-
+# # import os
+# # import argparse
+# # import fileinput
+# # import json
+# # import sys
+
+# # def replace_in_file(filename, search, replacement):
+# #     with fileinput.FileInput(filename, inplace=True, backup='.test') as file:
+# #         for line in file:
+# #             print(line.replace(rf"{search}", rf"{replacement}"), end='')
+
+
+# # def main():
+# #     parser = argparse.ArgumentParser()
+# #     parser.add_argument('files', nargs='*', help='Files to perform search and replace')
+# #     parser.add_argument('--search', help='Text to search for')
+# #     parser.add_argument('--replacement', help='Text to replace with')
+# #     parser.add_argument('--read-from-file', type=bool, default=False, help='Read search and replacement strings from file')
+# #     args = parser.parse_args()
+
+# #     if args.read_from_file:
+# #         # Read .project-properties.json from the current working directory
+# #         with open(os.path.join(os.getcwd(), '.project-properties.json'), 'r') as f:
+# #             replacements = json.load(f)
+# #             for filename in args.files:
+# #                 for replacement in replacements:
+# #                     replace_in_file(filename, replacement['search'], replacement['replacement'])
+# #     else:
+# #         for filename in args.files:
+# #             replace_in_file(filename, args.search, args.replacement)
+
 # import os
 # import argparse
 # import fileinput
@@ -10,7 +41,6 @@
 #         for line in file:
 #             print(line.replace(rf"{search}", rf"{replacement}"), end='')
 
-
 # def main():
 #     parser = argparse.ArgumentParser()
 #     parser.add_argument('files', nargs='*', help='Files to perform search and replace')
@@ -20,15 +50,26 @@
 #     args = parser.parse_args()
 
 #     if args.read_from_file:
-#         # Read .project-properties.json from the current working directory
-#         with open(os.path.join(os.getcwd(), '.project-properties.json'), 'r') as f:
-#             replacements = json.load(f)
-#             for filename in args.files:
-#                 for replacement in replacements:
-#                     replace_in_file(filename, replacement['search'], replacement['replacement'])
+#         try:
+#             # Read .project-properties.json from the current working directory
+#             with open(os.path.join(os.getcwd(), '.project-properties.json'), 'r') as f:
+#                 replacements = json.load(f)
+#         except FileNotFoundError:
+#             print("Error: .project-properties.json file not found.")
+#             sys.exit(1)
+#         except json.JSONDecodeError:
+#             print("Error: .project-properties.json is not a valid JSON file.")
+#             sys.exit(1)
+
+#         for filename in args.files:
+#             for replacement in replacements:
+#                 replace_in_file(filename, replacement['search'], replacement['replacement'])
 #     else:
 #         for filename in args.files:
 #             replace_in_file(filename, args.search, args.replacement)
+
+# if __name__ == "__main__":
+#     main()
 
 import os
 import argparse
@@ -46,19 +87,19 @@ def main():
     parser.add_argument('files', nargs='*', help='Files to perform search and replace')
     parser.add_argument('--search', help='Text to search for')
     parser.add_argument('--replacement', help='Text to replace with')
-    parser.add_argument('--read-from-file', type=bool, default=False, help='Read search and replacement strings from file')
+    parser.add_argument('--use-replacements-file', type=bool, default=False, help='Use search and replacement strings from replacements file')
+    parser.add_argument('--replacements-file', default='replacements.json', help='Path to the replacements file')
     args = parser.parse_args()
 
-    if args.read_from_file:
+    if args.use_replacements_file:
         try:
-            # Read .project-properties.json from the current working directory
-            with open(os.path.join(os.getcwd(), '.project-properties.json'), 'r') as f:
+            with open(os.path.join(os.getcwd(), args.replacements_file), 'r') as f:
                 replacements = json.load(f)
         except FileNotFoundError:
-            print("Error: .project-properties.json file not found.")
+            print(f"Error: {args.replacements_file} file not found.")
             sys.exit(1)
         except json.JSONDecodeError:
-            print("Error: .project-properties.json is not a valid JSON file.")
+            print(f"Error: {args.replacements_file} is not a valid JSON file.")
             sys.exit(1)
 
         for filename in args.files:
